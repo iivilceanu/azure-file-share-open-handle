3f439cee55c470f420fa2248b39bd4186b1d8aa5
iliev1 <71116646+iliev1@users.noreply.github.com>
2021-04-09 15:43:46 +0300

    Update get open handles.ps1

--- get open handles.ps1
+++ get open handles.ps1
@@ -2,6 +2,6 @@
 Select-AzSubscription -subscriptionid "17e9205a-8948-4a20-bb06-e19bf176af49"
 
 #create azure storage account context
-$Context = New-AzStorageContext -StorageAccountName "fcsb" -StorageAccountKey "FkE3eaAb7VoNxKYjEIBsb1W+gCoaIT1N7BK9fjhaoE2ZouPWW15FfSgEydSog+QKlpgjoKZsh4Eq/NrI9pQd6w=="
+$Context = New-AzStorageContext -StorageAccountName "fcsb" -StorageAccountKey ""
 
 Get-AzStorageFileHandle -Context $Context -ShareName "tier" -Recursive | Sort-Object ClientIP,OpenTime
